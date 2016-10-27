7fa109b99f0b43b332bedac9fd29feae6eeb8272
crisponions <crisponions@gmail.com>
2016-10-26 17:07:56 -0700

    more stuff

--- switch_tool/switch_tool_app.py
+++ switch_tool/switch_tool_app.py
@@ -2,8 +2,9 @@
 
 import socket
 import base64
-from .cisco_switch_tool import *
+from . import cisco_switch_tool
 from .credentials import username, password
+import pdb
 
 device = {
     'username': base64.b64decode(username),
@@ -14,6 +15,15 @@ device = {
 
 # variable to hold mac once found
 mac = ''
+
+_CORES = {
+    '172.17': '172.17.0.2',
+    '172.18': '172.18.0.2',
+    '172.19': '172.19.0.2',
+    '172.20': '172.20.0.2',
+    '172.21': '172.21.0.2',
+    '172.22': '172.22.0.2',
+}
 
 def validate_ip(s):
             a = s.split('.')
@@ -30,12 +40,20 @@ def validate_ip(s):
 # MOVE THIS FUNCTIONALITY TO WEB
 def find_core(ip_addr):
     a = ip_addr.split('.')
-    return '172.{}.0.2'.format(a[1]) 
+    ip = a[0] + '.' + a[1]
+    return _CORES[ip]
 
 def find_port(ip_addr):
     device['ip'] = find_core(ip_addr)
+    if device['ip'] == '172.20.0.2':
+        device['device_type'] = 'juniper'
+        return _juniper_find_port(ip_addr)
+    else:
+        return _cisco_find_port(ip_addr)   
+
+def _cisco_find_port(ip_addr):
     switch_ip = device['ip']
-    switch = CiscoSwitchTool(**device)
+    switch = cisco_switch_tool.CiscoSwitchTool(**device)
     mac = switch.mac_from_ip(ip_addr)
     if mac == None:
         return (None, None, None)
@@ -45,41 +63,31 @@ def find_port(ip_addr):
         ports = switch.ports_from_etherchannel(port[2:])
         name, switch_ip = switch.find_cdp_neighbor(ports[0])
         device['ip'] = switch_ip
-        switch = CiscoSwitchTool(**device)
+        switch = cisco_switch_tool.CiscoSwitchTool(**device)
         port = switch.port_from_mac(mac)
     if name is not 'CORE' and name is not None:
         device['ip'] = switch_ip
-        switch = CiscoSwitchTool(**device)
+        switch = cisco_switch_tool.CiscoSwitchTool(**device)
         port = switch.port_from_mac(mac)
     return (name, switch_ip, port)
 
-### REMOVE ME
-#
-#ip_add = input('Enter an IP address: ')
-#
-###
-'''
-ip_add=''
-choice = ''
-while choice.lower() not in ['i','n']:
-    choice = input("Search by (N)ame or (I)P [n/i]: ")
+def _juniper_find_port(ip_addr):
+    switch_ip = device['ip']
+    switch = juniper_switch_tool.JuniperSwitchTool(**device)
+    mac = switch.mac_from_ip(ip_addr)
+    if mac == None:
+        return (None, None, None)
+    port = switch.port_from_ip(ip_addr)
+    name = switch.get_switch_name()
+    while port.startswith('Po'):
+        ports = switch.ports_from_etherchannel(port[2:])
+        name, switch_ip = switch.find_neighbor(ports[0])
+        device['ip'] = switch_ip
+        switch = juniper_switch_tool.JuniperSwitchTool(**device)
+        port = switch.port_from_mac(mac)
+    if name is not 'CORE' and name is not None:
+        device['ip'] = switch_ip
+        switch = juniper_switch_tool.JuniperSwitchTool(**device)
+        port = switch.port_from_mac(mac)
+    return (name, switch_ip, port)
 
-    if choice.lower() == 'i':
-        ip_add = input('Enter an IP address: ')
-        # Looping until valid IP entered
-        while (validate_ip(ip_add) ==  False):
-            print(ip_add + ' is not a valid IPv4 address\n')
-            ip_add = input('Enter an IP address: ')
-    else:
-        name = input('Enter hostname: ')
-        try:
-            ip_add = socket.gethostbyname(name)
-        except:
-            ip_add = None
-
-if ip_add != None:
-    print(find_port(ip_add))
-else:
-    print("Device not found")
-
-'''
